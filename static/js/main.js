f123ea84f21aa647180a465ac13938828f8d5aea
jsad1900 <67195587+jsad1900@users.noreply.github.com>
2020-09-13 08:30:44 +0700

    add fb comment, google anystic

--- static/js/main.js
+++ static/js/main.js
@@ -58,4 +58,27 @@
       buttonCheck[i].appendChild(new_row);
     }
   }
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '325462015393237',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v8.0'
+  });
+    
+  FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+ FB.AppEvents.logPageView();
 })();
