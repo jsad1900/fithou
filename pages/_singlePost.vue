5bab07fc82d585e0f562397700dd368b7ecf628a
jsad1900 <67195587+jsad1900@users.noreply.github.com>
2020-10-21 18:13:34 +0700

    df

--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -22,7 +22,9 @@
             <p>
               Nếu có bất kỳ thắc mắc về bài thực hành, bài tập lớn, chương trình
               học, có thể liên hệ với anh tại:
-              <a href="https://www.facebook.com/ngocdanh0508">facebook Ngọc Danh</a>
+              <a href="https://www.facebook.com/ngocdanh0508"
+                >facebook Ngọc Danh</a
+              >
             </p>
           </div>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/prism.js"></script>
